5f04a0e18cb0430d1089c163feaa5685c4c741e6
p0n <p0n@necologylife.com>
2018-01-06 00:43:52 +0900

    Add .editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,18 @@
 module.exports = {
-    entry: __dirname + "/src/main.js",
-    output: {
-      path: __dirname + "/dist",
-      filename: "like-button.js"
-    },
-    module: {
-      loaders: [
-        {
-            test: /\.js(x?)$/,
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            query: {
-                presets: ["es2015", "stage-0", "react"]
-            }
+  entry: __dirname + "/src/main.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "like-button.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js(x?)$/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015", "stage-0", "react"]
         }
-      ]
-    }
+      }
+    ]
+  }
 };
